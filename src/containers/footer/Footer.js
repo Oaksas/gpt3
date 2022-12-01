d48ece758259f63f4325368f15bc61f928deb6f8
Oaksas <aman.getnet2@gmail.com>
2022-12-01 10:07:34 +0300

    Fix Footer

--- src/containers/footer/Footer.js
+++ src/containers/footer/Footer.js
@@ -2,50 +2,51 @@ import React from "react";
 import gpt3Logo from "../../assets/logo.svg";
 import "./footer.css";
 
-const Footer = () => (
-  <div className="gpt3__footer section__padding">
-    <div className="gpt3__footer-heading">
-      <h1 className="gradient-text">
-        Do you want to step in to the future before others
-      </h1>
-    </div>
-
-    <div className="gpt3__footer-btn">
-      <p>Request Early Access</p>
-    </div>
-
-    <div className="gpt3__footer-links">
-      <div className="gpt3__footer-links_logo">
-        <img src={gpt3Logo} alt="gpt3_logo" />
-        <p>
-          Crechterwoord K12 182 DK Alknjkcb, <br /> All Rights Reserved
-        </p>
+export const Footer = () => {
+  const currentYear = new Date().getFullYear();
+  return (
+    <div className="gpt3__footer section__padding">
+      <div className="gpt3__footer-heading">
+        <h1 className="gradient-text">
+          Do you want to step in to the future before others
+        </h1>
       </div>
-      <div className="gpt3__footer-links_div">
-        <h4>Links</h4>
-        <p>Overons</p>
-        <p>Social Media</p>
-        <p>Counters</p>
-        <p>Contact</p>
+
+      <div className="gpt3__footer-btn">
+        <p>Request Early Access</p>
       </div>
-      <div className="gpt3__footer-links_div">
-        <h4>Company</h4>
-        <p>Terms & Conditions </p>
-        <p>Privacy Policy</p>
-        <p>Contact</p>
+
+      <div className="gpt3__footer-links">
+        <div className="gpt3__footer-links_logo">
+          <img src={gpt3Logo} alt="gpt3_logo" />
+          <p>
+            Woldeamanual Getnet, <br /> All Rights Reserved
+          </p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Company</h4>
+          <p>Terms & Conditions </p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Get in touch</h4>
+          <p>Addis Ababa, Ethiopia</p>
+          <p>+251 985 412 182</p>
+          <p>info@payme.net</p>
+        </div>
       </div>
-      <div className="gpt3__footer-links_div">
-        <h4>Get in touch</h4>
-        <p>Crechterwoord K12 182 DK Alknjkcb</p>
-        <p>085-132567</p>
-        <p>info@payme.net</p>
+
+      <div className="gpt3__footer-copyright">
+        <p>{currentYear} GPT-3. All rights reserved.</p>
       </div>
     </div>
-
-    <div className="gpt3__footer-copyright">
-      <p>@2021 GPT-3. All rights reserved.</p>
-    </div>
-  </div>
-);
-
-export default Footer;
+  );
+};
